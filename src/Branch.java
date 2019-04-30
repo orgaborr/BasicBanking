5afbb51c20d10d3b2e528857d0e6598a955d79e2
Gabor <orbangabeszb@gmail.com>
2019-04-30 14:53:42 +0200

    addTransactions method moved to Branches class from Customer

--- src/Branch.java
+++ src/Branch.java
@@ -3,6 +3,28 @@ import java.util.ArrayList;
 public class Branch {
 	private ArrayList<Customer> customers = new ArrayList<Customer>();
 	
+	public void addCustomer(Customer customer) {
+		customers.add(customer);
+		System.out.println("Customer account of " + customer.getName() + " has been successfully created \nwith an initial transaction of $" + customer.getBalance());
+	}
 	
-
+	public void addTransaction(Customer customer, double amount) {
+		if(amount == 0) {
+			System.out.println("Invalid transaction of value $0.0.");
+		}
+		if(amount < 0) {
+			if(Math.abs(amount) > customer.getBalance()) {
+				System.out.println("Insufficient funds. Current balance is = $" + customer.getBalance());
+			}
+		} else {
+			customer.getTransactions().add(amount);
+			customer.setBalance(customer.getBalance() + amount);
+			System.out.println("Transaction successful. Current balance is = $" + customer.getBalance());
+		}
+	}
+	
+	+listCustomers: void
+		optionally list transactions as well
+	
+	
 }
--- src/Customer.java
+++ src/Customer.java
@@ -10,29 +10,16 @@ public class Customer {
 		this.balance = initialTransaction;
 	}
 	
-	public void addTransaction(double amount) {
-		if(amount == 0) {
-			System.out.println("Invalid transaction of value $0.0.");
-		}
-		if(amount < 0) {
-			if(Math.abs(amount) <= balance) {
-				transactions.add(amount);
-				balance += amount;
-				System.out.println("Transaction successful. Current balance is = $" + balance);
-			} else {
-				System.out.println("Insufficient funds. Current balance is = $" + balance);
-			}
-		} else {
-			balance += amount;
-			System.out.println("Transaction successful. Current balance is = $" + balance);
-		}
-	}
-	
+		
 	public void listTransactions() {
 		for(int i=0; i<transactions.size(); i++) {
 			System.out.println((i+1) + ". $" + transactions.get(i));
 		}
 		System.out.println("Current balance is = $" + balance);
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
 	}
 
 	public String getName() {
