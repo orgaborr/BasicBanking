0d8fcfcaaa6bb0614716fa98e3d01cc63774fc09
Gabor <orbangabeszb@gmail.com>
2019-05-03 14:41:17 +0200

    Customer class methods added

--- src/Bank.java
+++ src/Bank.java
@@ -4,37 +4,6 @@ public class Bank {
 	private String name;
 	private ArrayList<Branch> branches = new ArrayList<Branch>();
 	
-	public Bank(String name) {
-		this.name = name;
-	}
 	
-	public void addBranch(String name) {
-		if(!branchExists(name)) {
-			Branch branch = new Branch(name);
-			branches.add(branch);
-			System.out.println("New branch created: " + name);
-		} else {
-			System.out.println("Branch already exists");
-		}
-	}
-	
-	private boolean branchExists(String name) {
-		for(int i=0; i<branches.size(); i++) {
-			if(branches.get(i).getName().equals(name)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public void listBranches() {
-		for(int i=0; i<branches.size(); i++) {
-			System.out.println((i+1) + ". " + branches.get(i).getName());
-		}
-	}
-	
-	public ArrayList<Branch> getBranches() {
-		return branches;
-	}
 
 }
--- src/Customer.java
+++ src/Customer.java
@@ -3,22 +3,21 @@ import java.util.ArrayList;
 public class Customer {
 	private String name;
 	private ArrayList<Double> transactions = new ArrayList<Double>();
-	private double balance;
 	
 	public Customer(String name, double initialTransaction) {
 		this.name = name;
-		this.balance = initialTransaction;
+		addTransaction(initialTransaction);
+	}
+	
+	public void addTransaction(double amount) {
+		transactions.add(amount);
+		System.out.println("Transaction of $" + amount + " was successfully made");
 	}
 	
 	public void listTransactions() {
 		for(int i=0; i<transactions.size(); i++) {
-			System.out.println((i+1) + ". $" + transactions.get(i));
+			System.out.println((i+1) + " . $" + transactions.get(i));
 		}
-		System.out.println("Current balance is = $" + balance);
-	}
-	
-	public void setBalance(double balance) {
-		this.balance = balance;
 	}
 
 	public String getName() {
@@ -27,10 +26,6 @@ public class Customer {
 
 	public ArrayList<Double> getTransactions() {
 		return transactions;
-	}
-	
-	public double getBalance() {
-		return balance;
 	}
 
 }
